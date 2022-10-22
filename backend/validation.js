11ac689e1c0354ad1442b0f914105af0a2e5276a
Dan <danstolyarov79@gmail.com>
2022-10-22 14:45:47 +0300

    init

--- backend/validation.js
+++ backend/validation.js
@@ -14,7 +14,7 @@ async function validRegister(user)
         return {valid : false, reason : "Длина номера телефона превышает 32 цифры"}
     if (telegram.length > 64)
         return {valid : false, reason : "Длина тега telegram превышает 64 символа"}
-    if (avatar.size > 10240)
+    if (avatar.size > 10485760)
         return {valid : false, reason : "Размер аватара превышает 10 МБайт"}
     const ExpReg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     if (!ExpReg.test(email))
